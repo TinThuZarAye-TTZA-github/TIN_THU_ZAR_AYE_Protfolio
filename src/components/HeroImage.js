47327621051b0cc4a63724aafc8c6cfa30eacd8a
TinThuZarAye-TTZA-github <tinthuzaraye.github12@gmail.com>
2023-07-04 18:23:24 -0700

    added

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -5,7 +5,7 @@ import NavBar from './NavBar';
 import TechStack from './TechStack';
 function HeroImage() {
   const resumeURL =
-    'https://drive.google.com/file/d/1WtTdyo7cUWkj763qZ__pgLwaMeFq7Yhr/view?usp=drive_link';
+    'https://drive.google.com/file/d/166O1bOz-ldFRIlFh1mu_ObqkKIwK3KKe/view?usp=drive_link';
   return (
     <div className='hero'>
       <NavBar />

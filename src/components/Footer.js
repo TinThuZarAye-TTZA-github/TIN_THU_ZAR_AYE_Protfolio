75c001c55e4117b37abe38b1c6916bcc5fbfa05e
Tin Thu Zar Aye <tinthuzaraye@tinthuzrayesmbp.lan>
2024-08-01 23:56:37 -0700

    about me

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -21,8 +21,8 @@ function Footer() {
         <div className='right'>
           <h4 style={{ color: 'black' }}>About me</h4>
           <p style={{ color: 'black', fontSize: '17px' }}>
-            My name is Tin Thu Zar Aye. I graduated with Bachelor's
-            in Computer Science from San Francisco State University. 
+            My name is Tin Thu Zar Aye.
+            I graduated with Bachelor's in Computer Science from San Francisco State University. 
             Currently, I am pursing Master's of Science in Artificial Intelligence at San Jose State University.
           </p>
 

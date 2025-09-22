43abd0e35a85a65f2678900e66924720d3b9e0ac
Tin Thu Zar Aye <tinthuzaraye@Tin-Thu-Zar-Ayes-MacBook-Pro.local>
2025-09-21 23:28:43 -0700

    edited the ContactMe.js

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,9 +1,12 @@
 import '../style/ContactMe.css';
-import { FaMailBulk, FaPhone, FaRegUser } from 'react-icons/fa';
+import { FaGithub, FaLinkedin, FaMailBulk, FaPhone, FaRegUser } from 'react-icons/fa';
 import EmailForm from './EmailForm';
 import NavBar from './NavBar';
 
 function ContactMe() {
+    let githubURL = 'https://github.com/TinThuZarAye-TTZA-github';
+    let linkedinURL = 'https://www.linkedin.com/in/tin-thu-zar-aye/';
+
   return (
     <div>
       <NavBar />
@@ -18,13 +21,81 @@ function ContactMe() {
           <p>tinthuzaraye.tin@gmail.com</p>
         </div>
         <div className='location-contact'>
-          <FaPhone size={20} style={{ marginRight: '1rem' }} />
+          <FaPhone size={20} style={{ marginRight: '1rem', marginBottom : "20px" }} />
           <p>415-529-8920</p>
         </div>
+        <div>
+          <h4 style={{
+            fontSize: "1.5rem", 
+            color : "#2271b1", 
+            textAlign : "center",
+            marginTop: "20px", 
+            marginBottom : "20px", 
+            }} >
+
+            About Me
+          </h4>
+          <p style={{
+            color : 'black', 
+            textAlign : "center", 
+            marginBottom : "20px",
+            }}>
+
+              My name is Tin Thu Zar Aye.
+            I graduated with Bachelor's in Computer Science from San Francisco State University
+            and I am currently pursing Master's of Science in Artificial Intelligence at San Jose State University.</p>
+
+          <p style={{color: "black", textAlign:"center"}}>
+            I am passionate about Software Development and continuously learning new technologies including AI-powered Web Development.
+            My curiosity extends into Artificial Intelligence and Machine Learning where I explore how these technologies can transform the way we build,
+            optimize and personalize web applications. I am eager to apply AI-driven approachs to create smarter, scalable and innovative solutions
+            that improve user experience and deliver meaningful impact.
+          </p>
+            
+            
+
+         <div
+         
+         style ={{
+
+            display:"flex",
+            justifyContent : "center",
+            flexDirection: "row",
+            alignItem: "center",
+            gap: "2rem",
+            marginTop: "3rem"
+          }}>
+
+          
+
+          <FaGithub 
+         size={35}
+        style={{color: "black" , justifyContent: "center"}}
+        onClick={() => {
+          window.open(githubURL, '_blank')
+        }}
+         ></FaGithub>
+            
+         <FaLinkedin
+         size={35}
+         style={{color: "#0072B1"}}
+         onClick={() => {
+          window.open(linkedinURL, '_blank')
+         }}
+         >
+          
+         </FaLinkedin>
+
+         </div>
+
+         
+         
+         
+        </div>
       </div>
-      <div>
+      {/* <div>
         <EmailForm />
-      </div>
+      </div> */}
     </div>
   );
 }
--- src/components/EmailForm.js
+++ src/components/EmailForm.js
@@ -1,119 +1,119 @@
-import { useState } from 'react';
-import '../style/EmailForm.css';
-import emailjs from 'emailjs-com';
-import * as yup from 'yup';
-function EmailForm() {
-  // const form = useRef();
+// import { useState } from 'react';
+// import '../style/EmailForm.css';
+// import emailjs from 'emailjs-com';
+// import * as yup from 'yup';
+// function EmailForm() {
+//   // const form = useRef();
 
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: '',
-  });
+//   const [formData, setFormData] = useState({
+//     name: '',
+//     email: '',
+//     message: '',
+//   });
 
-  const schema = yup.object().shape({
-    name: yup.string().required('Name field is required!'),
-    email: yup.string().email().required('Please Enter You Email'),
-    message: yup.string().required('Please Enter Message'),
-  });
+//   const schema = yup.object().shape({
+//     name: yup.string().required('Name field is required!'),
+//     email: yup.string().email().required('Please Enter You Email'),
+//     message: yup.string().required('Please Enter Message'),
+//   });
 
-  // const sendEmail = (event) => {
-  //   event.preventDefault();
-  //   schema
-  //     .validate(formData)
-  //     .then(() => {
-  //       emailjs
-  //         .sendForm(
-  //           'service_jeb2gfb',
-  //           'template_j709x1c',
-  //           event.target,
-  //           '9swg5YG680pTAgq4X'
-  //         )
-  //         .then(
-  //           (result) => {
-  //             alert('Email sent successfully');
-  //             setFormData({ name: '', email: '', message: '' });
-  //           },
-  //           (error) => {
-  //             alert('Error' + error.message);
-  //           }
-  //         );
-  //     })
-  //     .catch((error) => {
-  //       alert('Please fill all the requirement');
-  //     });
-  // };
+//   // const sendEmail = (event) => {
+//   //   event.preventDefault();
+//   //   schema
+//   //     .validate(formData)
+//   //     .then(() => {
+//   //       emailjs
+//   //         .sendForm(
+//   //           'service_jeb2gfb',
+//   //           'template_j709x1c',
+//   //           event.target,
+//   //           '9swg5YG680pTAgq4X'
+//   //         )
+//   //         .then(
+//   //           (result) => {
+//   //             alert('Email sent successfully');
+//   //             setFormData({ name: '', email: '', message: '' });
+//   //           },
+//   //           (error) => {
+//   //             alert('Error' + error.message);
+//   //           }
+//   //         );
+//   //     })
+//   //     .catch((error) => {
+//   //       alert('Please fill all the requirement');
+//   //     });
+//   // };
 
-  // const handleChange = (event) => {
-  //   setFormData({ ...formData, [event.target.name]: event.target.value });
-  // };
+//   // const handleChange = (event) => {
+//   //   setFormData({ ...formData, [event.target.name]: event.target.value });
+//   // };
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+//   // const sendEmail = (e) => {
+//   //   e.preventDefault();
 
-    schema
-      .validate(formData)
-      .then(() => {
-        emailjs
-          .sendForm(
-            'service_jeb2gfb',
-            'template_j709x1c',
-            e.target,
-            '9swg5YG680pTAgq4X'
-          )
-          .then(
-            (result) => {
-              alert('Successfully sent!!');
-              setFormData({ name: '', email: '', message: '' });
-            },
-            (error) => {
-              alert(error.message);
-            }
-          );
-      })
-      .catch((error) => {
-        alert('Please fill all the fields');
-      });
-  };
+//   //   schema
+//   //     .validate(formData)
+//   //     .then(() => {
+//   //       emailjs
+//   //         .sendForm(
+//   //           'service_jeb2gfb',
+//   //           'template_j709x1c',
+//   //           e.target,
+//   //           '9swg5YG680pTAgq4X'
+//   //         )
+//   //         .then(
+//   //           (result) => {
+//   //             alert('Successfully sent!!');
+//   //             setFormData({ name: '', email: '', message: '' });
+//   //           },
+//   //           (error) => {
+//   //             alert(error.message);
+//   //           }
+//   //         );
+//   //     })
+//   //     .catch((error) => {
+//   //       alert('Please fill all the fields');
+//   //     });
+//   // };
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
+//   const handleChange = (e) => {
+//     setFormData({ ...formData, [e.target.name]: e.target.value });
+//   };
 
-  return (
-    <div className='form'>
-      <form onSubmit={sendEmail}>
-        <header className='email-me'>Reach Me Here!</header>
-        <label className='label'>Your Name</label>
-        <input
-          type='text'
-          name='name'
-          placeholder='Please write your name'
-          value={formData.name}
-          onChange={handleChange}
-        ></input>
-        <label className='label'>Your Email</label>
-        <input
-          type='email'
-          name='email'
-          placeholder='Please write your email'
-          value={formData.email}
-          onChange={handleChange}
-        ></input>
-        <label className='label'>Message</label>
-        <textarea
-          type='text'
-          rows='6'
-          placeholder='Please write your message'
-          name='message'
-          value={formData.message}
-          onChange={handleChange}
-        ></textarea>
-        <input type='submit' value='SEND!' className='btn'></input>
-      </form>
-    </div>
-  );
-}
-export default EmailForm;
+//   return (
+//     <div className='form'>
+//       <form onSubmit={sendEmail}>
+//         <header className='email-me'>Reach Me Here!</header>
+//         <label className='label'>Your Name</label>
+//         <input
+//           type='text'
+//           name='name'
+//           placeholder='Please write your name'
+//           value={formData.name}
+//           onChange={handleChange}
+//         ></input>
+//         <label className='label'>Your Email</label>
+//         <input
+//           type='email'
+//           name='email'
+//           placeholder='Please write your email'
+//           value={formData.email}
+//           onChange={handleChange}
+//         ></input>
+//         <label className='label'>Message</label>
+//         <textarea
+//           type='text'
+//           rows='6'
+//           placeholder='Please write your message'
+//           name='message'
+//           value={formData.message}
+//           onChange={handleChange}
+//         ></textarea>
+//         <input type='submit' value='SEND!' className='btn'></input>
+//       </form>
+//     </div>
+//   );
+// }
+// export default EmailForm;
 
-//service_ckd9c1z
+// //service_ckd9c1z
